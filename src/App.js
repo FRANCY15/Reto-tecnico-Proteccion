0b022836fbda95a3c7bc13a457ab958e075865e8
Francy Corredor <fran_ymile@hotmail.com>
2022-08-08 10:31:54 -0500

    prueba técnica Protección, ejercicio Fibonacci

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [semilla, setSemilla] = useState();
+  const [repeticiones, setRepeticiones] = useState();
+  const [resultado, setResultado] = useState([]);
+  const hora = new Date();
+  
+  
+  const handleChange = () => {
+    const captSemilla = hora.getMinutes();
+    setSemilla(captSemilla)
+    const captRepeticiones = hora.getSeconds();
+    setRepeticiones(captRepeticiones)
+    calcularFibonacci()
+  }
+  
+  const calcularFibonacci = () => {
+    let nums = num =>  Number(num);
+    let numsArray = Array.from(String(semilla), nums)
+    let arrayResult = [];
+    arrayResult.push(numsArray[0], numsArray[1])
+    console.log(arrayResult)
+    for(let i= 0; i <= repeticiones; i++){
+      arrayResult.push(arrayResult[arrayResult.length-1] + arrayResult[arrayResult.length-2])
+    }
+    setResultado(arrayResult)
+    console.log(arrayResult)
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='contenedorPrincipal'>
+      <div className='containerInformacion'>
+        <p className='informacion'>La hora es: {hora.toLocaleTimeString()}</p>
+        <p className='informacion'>La semilla es: {semilla}</p>
+        <p className='informacion'>La cantidad de repeticiones es: {repeticiones}</p>
+      </div>
+      <div className='containerInformacion'>
+        <p className='resultado'>{resultado.join(',')}</p>
+      </div>
+      <button className='btnCalcular' onClick={() => handleChange()}>Calcular serie Fibonacci</button>
     </div>
+    </>
   );
 }
 
